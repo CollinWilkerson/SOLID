259e1a944338934a8105936045b2938ecc81b533
CollinWilkerson <156472381+CollinWilkerson@users.noreply.github.com>
2025-11-07 11:34:02 -0700

    Add Dot Product selection

--- SOLID Project/Assets/_Project/Scripts/ResponsiveSelector.cs
+++ SOLID Project/Assets/_Project/Scripts/ResponsiveSelector.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class ResponsiveSelector : MonoBehaviour, ISelector
 {
-    [SerializeField] private float threshould;
-    [SerializeField] private Transform[] selectables;
+    [SerializeField] private float threshould = 0.99f;
+    [SerializeField] private SelectableObject[] selectables;
     private Transform selection;
+
+    private void Start()
+    {
+        selectables = FindObjectsByType<SelectableObject>(FindObjectsSortMode.None);
+    }
 
     public Transform GetSelection()
     {
@@ -15,12 +20,21 @@ public class ResponsiveSelector : MonoBehaviour, ISelector
     {
         selection = null;
 
-        foreach (Transform t in selectables)
+        float closest = 0;
+        foreach (SelectableObject t in selectables)
         {
             var vector1 = ray.direction;
-            var vector2 = t.position - ray.origin;
+            var vector2 = t.transform.position - ray.origin;
 
-            float lookPercentage = Vector3.Dot(vector1, vector2);
+            float lookPercentage = Vector3.Dot(vector1.normalized, vector2.normalized);
+
+            t.SetText(lookPercentage.ToString("F3"));
+
+            if(lookPercentage > threshould && lookPercentage > closest)
+            {
+                closest = lookPercentage;
+                selection = t.transform; 
+            }
         }
         return selection;
     }
